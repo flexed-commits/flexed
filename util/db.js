30c0c8f70456c250180cd6e44ba4b75b53e05ecc
flexed-commits <sm2828377@gmail.com>
2025-11-14 18:04:41 +0530

    Update db.js

--- util/db.js
+++ util/db.js
@@ -11,21 +11,13 @@ async function readData() {
     try {
         if (!fs.existsSync(DB_FILE)) {
             console.warn(`[DB] Database file not found at ${DB_FILE}. Creating empty structure.`);
-            return {
-                hierarchy: {},
-                resign_break_settings: {},
-                user_resignation_data: {}
-            };
+            return {};
         }
         const data = fs.readFileSync(DB_FILE, 'utf8');
         return JSON.parse(data);
     } catch (error) {
         console.error(`[DB] Error reading database file: ${error.message}. Returning empty object.`);
-        return {
-            hierarchy: {},
-            resign_break_settings: {},
-            user_resignation_data: {}
-        };
+        return {};
     }
 }
 
@@ -42,15 +34,4 @@ async function writeData(data) {
     }
 }
 
-/**
- * Checks if a specific guild has the basic resignation/break settings configured.
- * @param {string} guildId The ID of the guild.
- * @returns {Promise<boolean>} True if settings exist, false otherwise.
- */
-async function isGuildSetup(guildId) {
-    const data = await readData();
-    const settings = data.resign_break_settings[guildId];
-    return !!settings && !!settings.break_role && !!settings.resign_role && !!settings.break_resign_channel;
-}
-
-module.exports = { readData, writeData, isGuildSetup };
+module.exports = { readData, writeData };
